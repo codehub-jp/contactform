cac13080314fe3b4e9b82892027b79f3e2330c27
codehub-jp <codehub3110@gmail.com>
2023-02-04 10:37:31 +0900

    set contactform

---
+++ mail_confirm.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>お問合わせフォームを作る</title>
+    <link rel = "stylesheet" type="text/css" href="style2.css">
+</head>
+<body>
+    <h1>お問合わせの内容確認</h1>
+    <div class="confirm">
+        <p>お問い合わせ内容はこちらで宜しいでしょうか？ <br>
+        よろしければ「送信する」ボタンを押して下さい。
+        </p>
+        <div>
+            <p>名前</p>
+            <br>
+            <?php echo $_POST['name']; ?>
+        </div>
+
+        <div>
+            <p>メールアドレス</p>
+            <br>
+            <?php echo $_POST['mail']; ?>
+        </div>
+        <div>
+            <p>年齢</p>
+            <br>
+            <?php echo $_POST['age']."歳"; ?>
+        </div>
+        <div>
+            <p>コメント</p>
+            <br>
+            <?php echo $_POST['comments']; ?>
+        </div>
+        <form action="index.html">
+        <input type="submit" class="button1" value="戻って修正する" />
+        </form>
+
+        <form action="insert.php" method="post">
+            <input type="submit" class="button2"value="登録する" />
+            <input type="hidden" value="<?php echo $_POST['name']; ?>" name="name">
+            <input type="hidden" value="<?php echo $_POST['mail']; ?>" name="mail">
+            <input type="hidden" value="<?php echo $_POST['age']; ?>" name="age">
+            <input type="hidden" value="<?php echo $_POST['comments']; ?>" name="comments">
+        </form>
+    </div>
+</body>
+
+</html>
